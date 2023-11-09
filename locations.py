843ffc178f5fddbad616711c79c8f83715ec1d61
Ryanoh28 <ryantcfc@outlook.com>
2023-11-09 14:10:18 +0000

    Locations.py added
    New locations

---
+++ locations.py
@@ -0,0 +1,6 @@
+from combat import fight_monster
+
+def enter_dark_forest(player):
+    print("\nYou cautiously step into the Dark Forest, ready for whatever dangers lurk within...\n")
+    
+    fight_monster(player)
