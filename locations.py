0ee238fcc825d4447cb15e610f5e558cccb9b90d
Ryanoh28 <ryantcfc@outlook.com>
2023-11-11 14:43:03 +0000

    monster based on location

--- locations.py
+++ locations.py
@@ -4,15 +4,16 @@ from utilities import clear_console
 def enter_dark_forest(player, shop):
     while True:
         clear_console()
-        print("\nYou are in the Dark Forest. What would you like to do?")
+        print("You are in the Dark Forest. What would you like to do?\n")
         choice = input("(H)unt Monsters or (R)eturn to Camp: ").lower().strip()
         clear_console()
 
         if choice == "h":
-            fight_monster(player, shop)
+            fight_monster(player, shop, "Dark Forest")  
         elif choice == "r":
             from camp import return_to_camp
             return_to_camp(player, shop)
-            break
+            break  
         else:
             print("\nInvalid choice. Please enter 'H' to hunt monsters or 'R' to return to camp.\n")
+
